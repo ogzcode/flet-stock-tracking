bf27d36db95081cbced6401471a01aee2a1dd72c
ogzCode <oguzguc44@gmail.com>
2026-01-01 14:52:45 +0300

    feat: refactor login view to use local variables for username and password fields

--- views/login.py
+++ views/login.py
@@ -2,33 +2,51 @@ import flet as ft
 
 
 def login_view(page: ft.Page):
+    user_name = ft.TextField(label="Username", width=300, hint_text="Enter your username")
+    password = ft.TextField(
+        label="Password",
+        width=300,
+        password=True,
+        can_reveal_password=True,
+        hint_text="Enter your password",
+    )
+
+    def login_click(e: ft.Event[ft.Button]):
+        print("Login button clicked")
+        if user_name.value == "admin" and password.value == "admin123":
+            page.go("/dashboard")
+        else:
+            snack = ft.SnackBar(
+                content=ft.Text("Invalid username or password"),
+                bgcolor=ft.Colors.RED,
+            )
+            page.show_dialog(snack)
+        page.update()
+
+    login_button = ft.Button(
+        content="Login",
+        on_click=login_click,
+        style=ft.ButtonStyle(bgcolor=ft.Colors.BLUE, color=ft.Colors.WHITE),
+    )
+
     return ft.View(
         route="/login",
         controls=[
-            ft.Column(
-                [
-                    ft.Icon(ft.Icons.LOCK, size=80, color=ft.Colors.BLUE),
-                    ft.Text("Login", size=30, weight=ft.FontWeight.BOLD),
-                    ft.TextField(
-                        label="Username", width=300, hint_text="Enter your username"
-                    ),
-                    ft.TextField(
-                        label="Password",
-                        width=300,
-                        password=True,
-                        can_reveal_password=True,
-                        hint_text="Enter your password",
-                    ),
-                    ft.Button(
-                        content="Login",
-                        style=ft.ButtonStyle(
-                            bgcolor=ft.Colors.BLUE, color=ft.Colors.WHITE
-                        ),
-                    ),
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=20,
+            ft.Container(
+                expand=True,
+                alignment=ft.Alignment.CENTER,
+                content=ft.Column(
+                    [
+                        ft.Icon(ft.Icons.LOCK, size=80, color=ft.Colors.BLUE),
+                        ft.Text("Login", size=30, weight=ft.FontWeight.BOLD),
+                        user_name,
+                        password,
+                        login_button,
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=20,
+                ),
             )
         ],
     )
